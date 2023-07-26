775ffa236a2d44d6f599df84c6b16e7a9a8e780f
Aryya Bagus Padmanawijaya <aryyawijaya@gmail.com>
2023-07-26 08:21:05 +0700

    feat/connect-postgres

--- bin/config.ts
+++ bin/config.ts
@@ -6,6 +6,13 @@ const configs = {
   server: {
     PORT: process.env.PORT,
   },
+  postgres: {
+    HOST: process.env.PGHOST,
+    USER: process.env.PGUSER,
+    DATABASE: process.env.PGDATABASE,
+    PASSWORD: process.env.PGPASSWORD,
+    PORT: process.env.PGPORT,
+  },
 };
 
 export default configs;
